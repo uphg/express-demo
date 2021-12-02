6d7be88bfdcc491258007f3963fe56f24710ba22
chen <1834805770@qq.com>
2021-12-02 23:03:40 +0800

    refactor: 将 Demo 改为 TS 运行

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,14 @@
-const express = require('express')
+import express from 'express'
+
 const app = express()
 const port = 4000
 
 app.get('/', (request, response) => {
-  response.send('Hello World!')
+  response.send('你好！')
 })
 
 app.get('/demo', (request, response) => {
-  response.send('Hello Demo!')
+  response.send('你好，Demo!')
 })
 
 app.listen(port, () => {
